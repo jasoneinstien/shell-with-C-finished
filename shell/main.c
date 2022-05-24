d2ac89ad76929ce731a0499e41a5433094a6d150
Jason <65122962+jasoneinstien@users.noreply.github.com>
2022-05-24 19:31:57 +0800

    Add files via upload

--- shell/main.c
+++ shell/main.c
@@ -16,6 +16,64 @@ char *lsh_read_line(void);
 char **lsh_split_line(char*line);
 int lsh_launch(char**args);
 int lsh_execute(char **args);
+int lsh_cd(char **args);
+int lsh_help(char ** args);
+int lsh_exit(char ** args);
+
+//list of build in command and follow by then name of their function 
+//for ls help ==> a foward define
+//here use a pointer because the function pass through builtin_str is pointer function ie ==> execute function 
+char *builtin_str[] = {
+    "cd" , 
+    "help" , 
+    "exit" 
+};
+
+//address --> go to that function --> run that function 
+int(*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins(){
+    //how many build in function 
+    return sizeof(builtin_str)/sizeof(char*);
+}
+
+
+//build in function 
+//
+//if it's args == cd ==> go to lsh_cd function ==> because builtin_str[0] ==> & lsh_cd 
+int lsh_cd(char **args){
+    //if no argument ie)cd ==> ask users to give an argument 
+    if(args[1] == NULL){
+        fprintf(stderr , "lsh : expected argument to \"cd\" \n");
+    }else{
+        //other kind of error 
+        if(chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    //return true contineu the loop 
+    return 1; 
+}
+
+int lsh_help(char ** args){
+    int i;
+    printf("type cd to show the lsit\n");
+    printf("type exit to exit\n");
+
+    for(i = 0; i < lsh_num_builtins(); i++){
+        printf("%s \n" , builtin_str[i]);
+    }
+
+    return 1; 
+}
+
+int lsh_exit(char **args){
+    return 0; 
+}
 
 int main(int argc , char ** argv ){
     //argc && argv --> load config file , 
@@ -134,18 +192,50 @@ char ** lsh_split_line(char* line){
 //when the function is called the operation system which should be in two system will run as parents and child
 //exec ==> replact the current running program with new one 
 int lsh_launch(char ** args){
+    //process id 
     pid_t pid , wpid;
     int status;
-
+//fork incild two process 
     pid = fork();
-
+    //in the child loop (elimiarte the id with the main boot
     if(pid == 0){
-    
+        //basically run the program in side the program --> then allow the function to search the path 
+        //v represent vector --> args[0] , and argument with args
+        //when it return -1 it is an error
+        if(execvp(args[0] , args) == -1){
+            perror("lsh");
+        }   
+        exit(EXIT_FAILURE);
+     }//check fork error or not 
+    else if(pid < 0){
+        perror("lsh");
+    }else{
+        do{
+        //let parent waiting the command to finish running
+        wpid = waitpid(pid , &status , WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+
+    return 1; 
 }
 
 
+int lsh_execute(char** args){
+    int i ; 
+    if(args[0] == NULL){
+        //empty command
+        return 1; 
+    }
+    //loop and compare two function if equal == 0 note that because we save the char as int differnt patter will form differnt number unless they contin same character ie cd and dc 
+    for(i = 0 ; i < lsh_num_builtins(); i++){
+        if(strcmp(args[0] , builtin_str[i]) == 0){
+            return(*builtin_func[i])(args);
+        }
+    }
 
+    //loop the launch function again
+    return lsh_launch(args);
+}
 
 
 
